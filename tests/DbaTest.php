978adb4dc5c26e5f60796c660fd4fbcada933de0
Wasseem Khayrattee <w@7php.com>
2021-05-11 01:40:28 +0400

    feat: completed full unit test with 100% coverage

--- tests/DbaTest.php
+++ tests/DbaTest.php
@@ -38,15 +38,6 @@ test('open_persistent() - returned a resource handle to a persistent opened conn
 });
 
 /**
- * Testing Close()
- */
-test('Close() - When Closing the connection, the handler is indeed null', function () {
-    $this->dbaObject->open();
-    $this->dbaObject->close();
-    expect($this->dbaObject->resource_handle)->toBeNull();
-});
-
-/**
  * Testing fetch()
  */
 test('fetch() - retrieval of a value "hello world" for the key "123" should be successful', function () {
@@ -114,6 +105,79 @@ it('also allows for Insertion of a key string "this_is_a_key" and deleting it - 
 test('delete() - Deleting an unknown key "this_is_not_a_saved_key" - should return FALSE', function () {
     $this->dbaObject->open();
     expect($this->dbaObject->delete('this_is_not_a_saved_key'))->toBeFalse();
+});
+
+/**
+ * Testing createPathIfNone()
+ */
+test('createPathIfNone() - checking if the existing path "/var/www/storage" is there - returns TRUE', function () {
+    $newObject = getDbaObject();
+    $newObject->path = '/var/www/storage';
+    expect($newObject->createPathIfNone())->toBeTrue();
+});
+test('createPathIfNone() - will attempt to create unknown path "/var/www/storagexxx" and on success returns TRUE', function () {
+    $newObject = getDbaObject();
+    $newObject->path = '/var/www/storagexxx';
+    expect($newObject->createPathIfNone())->toBeTrue();
+    shell_exec('rm -rf /var/www/storagexxx');
+});
+test('createPathIfNone() - when a file is present at the given path, it will delete and recreate the folder for that path - returns TRUE', function () {
+    shell_exec('touch /var/www/storage_file');
+    $newObject = getDbaObject();
+    $newObject->path = '/var/www/storage_file';
+    expect($newObject->createPathIfNone())->toBeTrue();
+    shell_exec('rm -rf /var/www/storage_file');
+});
+
+/**
+ * Testing firstKey()
+ */
+test('firstKey() - retrieves the first key on an existing store - should return TRUE', function () {
+    $this->dbaObject->open();
+    expect($this->dbaObject->firstKey())->toEqual('123');
+});
+test('firstKey() - retrieving a key on an EMPTY store - should return FALSE', function () {
+    shell_exec('rm -rf /var/www/storage');
+    $this->dbaObject->open();
+    expect($this->dbaObject->firstKey())->toBeFalse();
+});
+
+/**
+ * Testing nextKey()
+ */
+test('nextKey() - retrieving the first key on an EMPTY store - should return FALSE', function () {
+    $this->dbaObject->open();
+    expect($this->dbaObject->nextKey())->toBeFalse();
+});
+test('nextKey() - retrieving the next key on an existing store - should return TRUE', function () {
+    $this->dbaObject->open();
+    $this->dbaObject->insert('1', 'hello world, this is 7PHP');
+    $this->dbaObject->insert('2', 'hello world, this is 7PHP - pass 02');
+    $who_cares_about_the_result = $this->dbaObject->firstKey();
+    expect($this->dbaObject->nextKey())->toEqual(2);
+});
+
+/**
+ * Testing replace()
+ */
+test('replace() - if successful, replacing value "hello world" for Key "44" by value "hello Mauritius" should return TRUE', function () {
+    $this->dbaObject->open();
+    $this->dbaObject->insert('44', 'hello world');
+    expect($this->dbaObject->replace('44', 'hello Mauritius'))->toBeTrue();
+});
+it('confirms the updated value "hello Mauritius" is indeed present', function () {
+    $this->dbaObject->open();
+    expect($this->dbaObject->replace('44', 'hello Mauritius'))->toBeTrue();
+    expect($this->dbaObject->fetch('44'))->toEqual('hello Mauritius');
+});
+
+/**
+ * Testing Close()
+ */
+test('Close() - When Closing the connection, the handler is indeed null', function () {
+    $this->dbaObject->open();
+    $this->dbaObject->close();
+    expect($this->dbaObject->resource_handle)->toBeNull();
 });
 
 /**
